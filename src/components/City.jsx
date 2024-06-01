042bfcdca9788c6031f65a87dcf5e5a2534735a6
Sladjana <csl.bunny.2207@gmail.com>
2024-06-01 15:40:44 +0200

    delete comments

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -29,17 +29,6 @@ function City() {
     [id]
   );
 
-  // function getFlag(flag) {
-  //   if (flag === undefined) return;
-  //
-  //   let countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-  //     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-  //     .join("");
-  //   return (
-  //     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
-  //   );
-  // }
-
   const { cityName, date, notes, emoji } = currentCity;
 
   if (isLoading) return <Spinner />;
