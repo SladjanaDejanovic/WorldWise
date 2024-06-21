cf19a3c89e591a613af52c9c29f69bdad4438dbd
Sladjana <csl.bunny.2207@gmail.com>
2024-06-21 20:52:37 +0200

    update

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -24,7 +24,7 @@ export default User;
 
 /*
 
-3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app`
+
 4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
 5) Handle logout button by calling `logout()` and navigating back to `/`
 */
